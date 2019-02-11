83f3595d4362b408c3320e6a62f5f8ff5d0811c3
foxman <lin.vladimir@gmail.com>
2019-02-11 08:44:05 +0800

    add some other color models

---
+++ src/cmyk.cpp
@@ -0,0 +1,17 @@
+#include "colors.hpp"
+#include "stdlib.h"
+#include "math.h"
+
+#pragma pack(push,1)
+
+
+#pragma pack(pop)
+
+#ifndef DONT_USE_NAMESPACE
+namespace COLORS_NAMESPACE {
+#endif
+
+
+#ifndef DONT_USE_NAMESPACE
+}
+#endif
